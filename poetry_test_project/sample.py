24ea7ba73081006ad85db4faec64b1d92a05777d
WSBTFC513 <tfca.wasabi@gmail.com>
2023-10-17 23:21:23 +0900

    settings.json更新

--- poetry_test_project/sample.py
+++ poetry_test_project/sample.py
@@ -2,7 +2,11 @@ import os
 import time
 
 
-def sample(a: int, b: bool) -> int:
+def sample(a: int, b: int) -> int:
     os.environ.get("ENV", "")
     time
     return a + b
+
+
+def sample2(x: str, y: str) -> str:
+    return x + y
